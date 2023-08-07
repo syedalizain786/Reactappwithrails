f159d1617cd479cc434c1d4b210605fd38f999ba
syedalizain <syedalizain70@gmail.com>
2023-08-07 23:37:23 +0500

    Local Storage Work

---
+++ src/components/Books.js
@@ -0,0 +1,29 @@
+const Books=[
+        {
+            "book_id": 6,
+            "title": "Book 1",
+            "summary": "ajksjlaldjadjladjklasjklda",
+            "date": "2023-10-23"
+        },
+        {
+            "book_id": 7,
+            "title": "Book 2",
+            "summary": "ajksjlaldjadjladjklasjklda",
+            "date": "2023-11-23"
+        },
+        {
+            "book_id": 8,
+            "title": "Book 3",
+            "summary": "ajksjlaldjadjladjklasjklda",
+            "date": "2023-12-23"
+        },
+        {
+            "book_id": 9,
+            "title": "Zain",
+            "summary": "asljdjlasjhdjlasjhdjlhasdlhdasjl",
+            "date": "2023-12-20"
+        }
+    ]
+
+
+export default Books;
